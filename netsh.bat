44187be5ea01cdb4599e35353a847365cc9be0a4
ken <119792185+devken0@users.noreply.github.com>
2024-01-28 18:42:15 +0800

    Update netsh.bat

--- netsh.bat
+++ netsh.bat
@@ -1,5 +1,8 @@
 REM show info about wifi
 netsh wlan show profile
 
-REM show password
-netsh wlan show profile name="wifiname" key=clear
+REM show info and password in clear text
+netsh wlan show profile name="" key=clear
+
+REM show password only
+netsh wlan show profile name="" key=clear | findstr Key
