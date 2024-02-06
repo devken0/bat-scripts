df19cd3f2fcaf18f221c41179e679ad37351f34e
ken <119792185+devken0@users.noreply.github.com>
2024-02-06 18:42:09 +0800

    Update vmstart.bat

--- vmstart.bat
+++ vmstart.bat
@@ -2,4 +2,6 @@ REM 3d acceleration must be disabled
 
 vmrun -T ws start "F:\04_Misc\Virtual-Machines\02_VMware\vm0\vm0.vmx" nogui
 
+vmrun -T ws start "F:\04_Misc\Virtual-Machines\02_VMware\vm1\vm1.vmx" nogui
 
+vmrun -T ws start "F:\04_Misc\Virtual-Machines\02_VMware\vm2\vm2.vmx" nogui
