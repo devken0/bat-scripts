c6ade49dbf2a670327b7dd79b17a66112457838a
ken <119792185+devken0@users.noreply.github.com>
2024-02-07 04:04:32 +0800

    Update vmstart.bat

--- vmstart.bat
+++ vmstart.bat
@@ -1,11 +1,5 @@
 REM 3d acceleration must be disabled
 
-REM vmrun -T ws start "F:\04_Misc\Virtual-Machines\02_VMware\vm0\vm0.vmx" nogui
-
-REM vmrun -T ws start "F:\04_Misc\Virtual-Machines\02_VMware\vm1\vm1.vmx" nogui
-
-REM vmrun -T ws start "F:\04_Misc\Virtual-Machines\02_VMware\vm2\vm2.vmx" nogui
-
-vmrun start "F:\04_Misc\Virtual-Machines\02_VMware\vm0\vm0.vmx" nogui
-vmrun start "F:\04_Misc\Virtual-Machines\02_VMware\vm1\vm1.vmx" nogui
-vmrun start "F:\04_Misc\Virtual-Machines\02_VMware\vm2\vm2.vmx" nogui
+C:\Program Files (x86)\VMware\VMware Player\vmrun.exe start "F:\04_Misc\Virtual-Machines\02_VMware\vm0\vm0.vmx" nogui
+C:\Program Files (x86)\VMware\VMware Player\vmrun.exe start "F:\04_Misc\Virtual-Machines\02_VMware\vm1\vm1.vmx" nogui
+C:\Program Files (x86)\VMware\VMware Player\vmrun.exe start "F:\04_Misc\Virtual-Machines\02_VMware\vm2\vm2.vmx" nogui
